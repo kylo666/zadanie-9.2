88a4ef8c2bb96d175d518e69ee4246846c6b254b
kylo666 <krzysztofhernas@gmail.com>
2017-10-19 19:33:48 +0200

    zadanie 9.2

---
+++ js/script.js
@@ -0,0 +1,10 @@
+﻿var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);  //  ['Asia', 'Kasia', 'Ola', 'Jola','Piotrek', 'Marek', 'Arek', 'Jarek' ];
+
+var newName = 'Marian'
+if (allNames.indexOf(newName) === -1) {
+allNames.push('Marian');
+	console.log('Wyswietl Tablice');}
